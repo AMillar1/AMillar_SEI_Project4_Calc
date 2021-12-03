e43c4f6a08512b06f8ebf2c8b59970b25db8c2a6
Adam Millar <adammillar@Adams-Air.hsd1.wa.comcast.net>
2021-12-03 14:43:44 -0800

    Add Button for digit and operation components. Try reducer to manage state

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -1,29 +1,57 @@
+import { useReducer } from "react"
 import "../Calculator/Calculator.css"
+import ButtonDigit from "../Button/ButtonDigit"
+import ButtonOperation from "../Button/ButtonOperation"
 
+
+export const ACTIONS = {
+    ADD_DIGIT: 'add-digit',
+    CHOOSE_OPERATION: 'choose-operation',
+    CLEAR: 'clear',
+    DELETE_DIGIT: 'delete-digit',
+    EVALUATE: 'evaluate',
+}
+
+
+function reducer(state, {type, payload}) {
+    switch(type) {
+        case ACTIONS.ADD_DIGIT:
+            if(payload.digit === "0" && state.currentOperand === "0") return state // if the current state is only a zero, don't add a zero. This prevents arbitrary beginning strings of zeros
+            if(payload.digit === "." && state.currentOperand.includes(".")) return state // if there is already a decimal point in the input, do not add another one. 
+            return {
+                ...state, 
+                currentOperand: `${state.currentOperand || ""}${payload.digit}`
+            }
+    }
+}
 export default function Calculator() {
+    const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer, {})
+    
+    // dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 1 }})
+
     return(
         <div className="calculator-grid">
             <div className="output">
-                <div className="previous-operand">PO</div>
-                <div className="current-operand">CO</div>
+                <div className="previous-operand">{previousOperand} {operation}</div>
+                <div className="current-operand">{currentOperand}</div>
             </div>
             <button className="span-two">AC</button>
             <button>DEL</button>
-            <button>/</button>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>*</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>+</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>-</button>
-            <button>.</button>
-            <button>0</button>
+            <ButtonOperation operation="/" dispatch={dispatch} />
+            <ButtonDigit digit="1" dispatch={dispatch} />
+            <ButtonDigit digit="2" dispatch={dispatch} />
+            <ButtonDigit digit="3" dispatch={dispatch} />
+            <ButtonOperation operation="*" dispatch={dispatch} />
+            <ButtonDigit digit="4" dispatch={dispatch} />
+            <ButtonDigit digit="5" dispatch={dispatch} />
+            <ButtonDigit digit="6" dispatch={dispatch} />
+            <ButtonOperation operation="+" dispatch={dispatch} />
+            <ButtonDigit digit="7" dispatch={dispatch} />
+            <ButtonDigit digit="8" dispatch={dispatch} />
+            <ButtonDigit digit="9" dispatch={dispatch} />
+            <ButtonOperation operation="-" dispatch={dispatch} />
+            <ButtonDigit digit="." dispatch={dispatch} />
+            <ButtonDigit digit="0" dispatch={dispatch} />
             <button className="span-two">=</button>
         </div>
     )
