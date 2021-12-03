f4001468f96af57ae00f095322ad07771dd049c5
Adam Millar <adammillar@Adams-Air.hsd1.wa.comcast.net>
2021-12-03 15:08:29 -0800

    Add operation switch and evaluate function

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -16,14 +16,63 @@ export const ACTIONS = {
 function reducer(state, {type, payload}) {
     switch(type) {
         case ACTIONS.ADD_DIGIT:
-            if(payload.digit === "0" && state.currentOperand === "0") return state // if the current state is only a zero, don't add a zero. This prevents arbitrary beginning strings of zeros
-            if(payload.digit === "." && state.currentOperand.includes(".")) return state // if there is already a decimal point in the input, do not add another one. 
+            if(payload.digit === "0" && state.currentOperand === "0") {
+                return state
+            } // if the current state is only a zero, don't add a zero. This prevents arbitrary beginning strings of zeros
+            if(payload.digit === "." && state.currentOperand.includes(".")) {
+                return state
+            } // if there is already a decimal point in the input, do not add another one. 
             return {
                 ...state, 
                 currentOperand: `${state.currentOperand || ""}${payload.digit}`
             }
+        case ACTIONS.CLEAR:
+            return {}
+        case ACTIONS.CHOOSE_OPERATION: 
+            if (state.currentOperand == null && state.previousOperand == null) {
+                return state // do nothing if the state is empty
+            }
+            if (state.previousOperand == null) {
+                return {
+                    ...state, //the state as it exists
+                    operation: payload.operation, //whatever button what clicked --that's what payload tracks
+                    previousOperand: state.currentOperand, //shift the current operand 'up'
+                    currentOperand: null //reset the current operand. 
+                }
+            }
+            return {
+                ...state, 
+                previousOperand: evaluate(state),
+                operation: payload.operation,
+                currentOperand: null,
+
+            }
     }
 }
+
+function evaluate({currentOperand, previousOperand, operation}) {
+    const previous = parseFloat(previousOperand)
+    const current = parseFloat(currentOperand)
+    if (isNaN(previous) ||  isNaN(current)) return "" //if any of these things is not a number, clear the fields. 
+    let computation = ""
+    switch (operation) {
+        case "+":
+            computation = previous + current
+            break
+        case "-":
+            computation = previous - current
+            break
+        case "*":
+            computation = previous * current
+            break
+        case "/":
+            computation = previous / current
+            break
+    }
+    return computation.toString()
+}
+
+
 export default function Calculator() {
     const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer, {})
     
@@ -35,7 +84,7 @@ export default function Calculator() {
                 <div className="previous-operand">{previousOperand} {operation}</div>
                 <div className="current-operand">{currentOperand}</div>
             </div>
-            <button className="span-two">AC</button>
+            <button className="span-two" onClick={() => dispatch({ type: ACTIONS.CLEAR })}>AC</button>
             <button>DEL</button>
             <ButtonOperation operation="/" dispatch={dispatch} />
             <ButtonDigit digit="1" dispatch={dispatch} />
